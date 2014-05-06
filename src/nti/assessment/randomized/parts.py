419e2fc96053316f9f2517f0071a1b265781289e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-06 21:20:24 +0000

    set __externa_class_name__
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@38541 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/randomized/parts.py
+++ src/nti/assessment/randomized/parts.py
@@ -16,8 +16,10 @@ from ..parts import QMultipleChoicePart
 
 @interface.implementer(interfaces.IQRandomizedMatchingPart)
 class QRandomizedMatchingPart(QMatchingPart):
+	__external_class_name__ = "MatchingPart"
 	grader_interface = interfaces.IQRandomizedMatchingPartGrader
 
 @interface.implementer(interfaces.IQRandomizedMultipleChoicePart)
 class QRandomizedMultipleChoicePart(QMultipleChoicePart):
+	__external_class_name__ = "MultipleChoicePart"
 	grader_interface = interfaces.IQRandomizedMultipleChoicePartGrader
