1c8165545f8ed408eb686e9647a7bfb92cbb9b04
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-07 15:55:35 +0000

    add test layer. still trying to figure out how to tell plastex the location of the commands
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@38578 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/tests/__init__.py
+++ src/nti/assessment/tests/__init__.py
@@ -7,11 +7,50 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from zope import component
+
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+import zope.testing.cleanup
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+								 ConfiguringLayerMixin):
+
+	set_up_packages = ('nti.contentrendering', 'nti.assessment', 'nti.externalization',
+					   'zope.annotation', 'nti.mimetype')
+
+	@classmethod
+	def setUp(cls):
+		cls.setUpPackages()
+
+	@classmethod
+	def tearDown(cls):
+		cls.tearDownPackages()
+		zope.testing.cleanup.cleanUp()
+
+	@classmethod
+	def testSetUp(cls):
+		pass
+
+	@classmethod
+	def testTearDown(cls):
+		pass
+
+from nti.contentrendering.tests import simpleLatexDocumentText
+
+def _simpleLatexDocument(maths):
+	return simpleLatexDocumentText(preludes=(br'\usepackage{ntiassessment}',),
+									bodies=maths)
+simpleLatexDocument = _simpleLatexDocument
+
+# ==========
+
 from hamcrest.core.base_matcher import BaseMatcher
 
 class GradeMatcher(BaseMatcher):
 	def __init__(self, value, response):
-		super(GradeMatcher,self).__init__()
+		super(GradeMatcher, self).__init__()
 		self.value = value
 		self.response = response
 
@@ -28,12 +67,12 @@ class GradeMatcher(BaseMatcher):
 
 		mismatch_description.append_text(' graded ' + repr(self.response) + ' as ' + str(not self.value))
 
-	def __repr__( self ):
+	def __repr__(self):
 		return 'solution that grades as ' + str(self.value)
 
-def grades_correct( response ):
+def grades_correct(response):
 	return GradeMatcher(True, response)
 grades_right = grades_correct
 
-def grades_wrong( response ):
-	return GradeMatcher(False, response )
+def grades_wrong(response):
+	return GradeMatcher(False, response)
