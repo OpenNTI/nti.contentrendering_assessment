eb6820a126c41dc8f591e5bcc6f227f230a75ff8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-07 16:20:39 +0000

    use layers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@38583 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/contentrendering/tests/test_randomized.py
+++ src/nti/assessment/contentrendering/tests/test_randomized.py
@@ -12,60 +12,51 @@ from hamcrest import has_length
 from hamcrest import assert_that
 from hamcrest import has_property
 
-import nti.contentrendering
+import unittest
 
-import nti.externalization
+from nti.assessment.randomized import interfaces as rand_interfaces
+from nti.assessment.contentrendering.ntiassessment import naquestion
 
-from ..ntiassessment import naquestion
-from ...randomized import interfaces as rand_interfaces
-
-from nti.contentrendering.tests import simpleLatexDocumentText
-from nti.contentrendering.tests import buildDomFromString as _buildDomFromString
-
-import nti.testing.base
 from nti.testing.matchers import verifiably_provides
 
-def _simpleLatexDocument(maths):
-	return simpleLatexDocumentText( preludes=(br'\usepackage{ntiassessment}',),
-									bodies=maths )
+from nti.assessment.contentrendering.tests import _simpleLatexDocument
+from nti.assessment.contentrendering.tests import SharedConfiguringTestLayer
 
-# Nose module-level setup and teardown
-setUpModule = lambda: nti.testing.base.module_setup(set_up_packages=(nti.contentrendering,
-																	 'nti.assessment',
-																	 nti.externalization))
-tearDownModule = nti.testing.base.module_teardown
+from nti.contentrendering.tests import buildDomFromString as _buildDomFromString
 
-def test_multiple_choice_macros():
-	example = br"""
-		\begin{naquestion}
-			Arbitrary prefix content goes here.
-			\begin{naqmultiplechoicepart}[randomize=true]
-			   Arbitrary content for this part goes here.
-			   \begin{naqchoices}
-			   		\naqchoice Arbitrary content for the choice.
-					\naqchoice[1] Arbitrary content for this choice; this is the right choice.
-					\naqchoice[0.5] This choice is half correct.
-				\end{naqchoices}
-				\begin{naqsolexplanation}
-					Arbitrary content explaining how the correct solution is arrived at.
-				\end{naqsolexplanation}
-			\end{naqmultiplechoicepart}
-		\end{naquestion}
-		"""
+class TestRandomized(unittest.TestCase):
 
-	dom = _buildDomFromString(_simpleLatexDocument((example,)))
-	assert_that(dom.getElementsByTagName('naquestion'), has_length(1))
-	assert_that(dom.getElementsByTagName('naquestion')[0], is_(naquestion))
+	layer = SharedConfiguringTestLayer
 
-	assert_that(dom.getElementsByTagName('naqchoice'), has_length(3))
-	assert_that(dom.getElementsByTagName('naqsolution'), has_length(2))
+	def test_multiple_choice_macros(self):
+		example = br"""
+			\begin{naquestion}
+				Arbitrary prefix content goes here.
+				\begin{naqmultiplechoicepart}[randomize=true]
+				   Arbitrary content for this part goes here.
+				   \begin{naqchoices}
+				   		\naqchoice Arbitrary content for the choice.
+						\naqchoice[1] Arbitrary content for this choice; this is the right choice.
+						\naqchoice[0.5] This choice is half correct.
+					\end{naqchoices}
+					\begin{naqsolexplanation}
+						Arbitrary content explaining how the correct solution is arrived at.
+					\end{naqsolexplanation}
+				\end{naqmultiplechoicepart}
+			\end{naquestion}
+			"""
 
-	naq = dom.getElementsByTagName('naquestion')[0]
-	part_el = naq.getElementsByTagName('naqmultiplechoicepart')[0]
-	assert_that(part_el, has_property('randomize', is_(True)))
+		dom = _buildDomFromString(_simpleLatexDocument((example,)))
+		assert_that(dom.getElementsByTagName('naquestion'), has_length(1))
+		assert_that(dom.getElementsByTagName('naquestion')[0], is_(naquestion))
 
-	part = part_el.assessment_object()
-	assert_that(part, verifiably_provides(part_el.part_interface))
-	assert_that(part_el.part_interface, is_(rand_interfaces.IQRandomizedMultipleChoicePart))
+		assert_that(dom.getElementsByTagName('naqchoice'), has_length(3))
+		assert_that(dom.getElementsByTagName('naqsolution'), has_length(2))
 
-	
+		naq = dom.getElementsByTagName('naquestion')[0]
+		part_el = naq.getElementsByTagName('naqmultiplechoicepart')[0]
+		assert_that(part_el, has_property('randomize', is_(True)))
+
+		part = part_el.assessment_object()
+		assert_that(part, verifiably_provides(part_el.part_interface))
+		assert_that(part_el.part_interface, is_(rand_interfaces.IQRandomizedMultipleChoicePart))
