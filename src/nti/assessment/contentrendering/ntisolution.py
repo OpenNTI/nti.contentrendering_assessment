d04c1a6d5d52eb1a2889f898d9d695db070c97fe
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-04 22:11:05 +0000

    move some solutions  based elements to a module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@52874 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/assessment/contentrendering/ntisolution.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from plasTeX import Base
+
+from .ntibase import _LocalContentMixin
+
+# Handle custom counter names
+
+class naquestionsetname(Base.Command):
+	unicode = ''
+
+class naquestionname(Base.Command):
+	unicode = ''
+
+class naqsolutionnumname(Base.Command):
+	unicode = ''
+
+class naqsolutions(Base.List):
+
+	counters = ['naqsolutionnum']
+	args = '[ init:int ]'
+
+	def invoke( self, tex ):
+		# TODO: Why is this being done?
+		res = super(naqsolutions, self).invoke( tex )
+
+		if 'init' in self.attributes and self.attributes['init']:
+			self.ownerDocument.context.counters[self.counters[0]].setcounter(self.attributes['init'])
+		elif self.macroMode != Base.Environment.MODE_END:
+			self.ownerDocument.context.counters[self.counters[0]].setcounter(0)
+
+		return res
+
+	def digest( self, tokens ):
+		# After digesting loop back over the children moving nodes before
+		# the first item into the first item
+		# TODO: Why is this being done?
+		res = super(naqsolutions, self).digest(tokens)
+		if self.macroMode != Base.Environment.MODE_END:
+			nodesToMove = []
+
+			for node in self:
+
+				if isinstance(node, Base.List.item):
+					nodesToMove.reverse()
+					for nodeToMove in nodesToMove:
+						self.removeChild(nodeToMove)
+						node.insert(0, nodeToMove)
+					break
+
+				nodesToMove.append(node)
+
+		return res
+
+_LocalContentMixin._asm_ignorable_renderables += (naqsolutions,)
+
+class naqsolution(Base.List.item):
+
+	args = '[weight:float] <units>'
+
+	# We use <> for the units list because () looks like a geometric
+	# point, and there are valid answers like that.
+	# We also do NOT use the :list conversion, because if the units list
+	# has something like an (escaped) % in it, plasTeX fails to tokenize the list
+	# Instead, we work with the TexFragment object ourself
+
+	def invoke( self, tex ):
+		# TODO: Why is this being done? Does the counter matter?
+		self.counter = naqsolutions.counters[0]
+		self.position = self.ownerDocument.context.counters[self.counter].value + 1
+		#ignore the list implementation
+		return Base.Command.invoke(self,tex)
+
+	def units_to_text_list(self):
+		"""Find the units, if any, and return a list of their text values"""
+		units = self.attributes.get( 'units' )
+		if units:
+			# Remove trailing delimiter and surrounding whitespace. For consecutive
+			# text parts, we have to split ourself
+			result = []
+			for x in units:
+				# We could get elements (Macro/Command) or strings (plastex.dom.Text)
+				if getattr( x, 'tagName', None ) == 'math':
+					raise ValueError( "Math cannot be roundtripped in units. Try unicode symbols" )
+				x = unicode(x).rstrip( ',' ).strip()
+				result.extend( x.split( ',' ) )
+			return result
+
+	def units_to_html(self):
+		units = self.units_to_text_list()
+		if units:
+			return ','.join( units )
+
+_LocalContentMixin._asm_ignorable_renderables += (naqsolution,)
+
+###
+# Explanations
+###
+
+class naqsolexplanation(_LocalContentMixin, Base.Environment):
+	pass
+
+_LocalContentMixin._asm_ignorable_renderables += (naqsolexplanation,)
