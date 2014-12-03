34958c71cd05b561f0c777a5b72c239eaa162cd7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-12-03 23:31:58 +0000

    use new draw attribute in ranges to draw questions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@55016 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/randomized/__init__.py
+++ src/nti/assessment/randomized/__init__.py
@@ -24,56 +24,56 @@ from nti.dataserver.interfaces import IUser
 from .interfaces import ISha224Randomized
 
 def get_current_user():
-    interaction = queryInteraction()
-    participations = list(getattr(interaction, 'participations', None) or ())
-    participation = participations[0] if participations else None
-    principal = getattr(participation, 'principal', None)
-    return principal.id if principal is not None else None
+	interaction = queryInteraction()
+	participations = list(getattr(interaction, 'participations', None) or ())
+	participation = participations[0] if participations else None
+	principal = getattr(participation, 'principal', None)
+	return principal.id if principal is not None else None
 
 def get_user(user=None):
-    user = get_current_user() if user is None else user
-    if user is not None and not IUser.providedBy(user):
-        user = User.get_user(str(user))
-    return user
+	user = get_current_user() if user is None else user
+	if user is not None and not IUser.providedBy(user):
+		user = User.get_user(str(user))
+	return user
 
 def randomize(user=None, context=None):
-    user = get_user(user)        
-    if user is not None:
-        uid = component.getUtility(IIntIds).getId(user)
-        use_sha224 = context is not None and ISha224Randomized.providedBy(context)
-        if use_sha224:
-            hexdigest = hashlib.sha224(bytes(uid)).hexdigest()
-            generator = random.Random(long(hexdigest, 16))
-        else:
-            generator = random.Random(uid)
-        return generator
-    return None
+	user = get_user(user)		
+	if user is not None:
+		uid = component.getUtility(IIntIds).getId(user)
+		use_sha224 = context is not None and ISha224Randomized.providedBy(context)
+		if use_sha224:
+			hexdigest = hashlib.sha224(bytes(uid)).hexdigest()
+			generator = random.Random(long(hexdigest, 16))
+		else:
+			generator = random.Random(uid)
+		return generator
+	return None
 
 def shuffle_list(generator, target):
-    generator.shuffle(target)
-    return target
+	generator.shuffle(target)
+	return target
 
 def questionbank_random(context, user=None):
-    generator = random.Random() if context.srand else randomize(user=user, context=context)
-    return generator
+	generator = random.Random() if context.srand else randomize(user=user, context=context)
+	return generator
 
 def questionbank_question_chooser(context, questions=None, user=None):
-    result = []
-    questions = questions or context.questions
-    generator = questionbank_random(context, user=user)
-    if generator and questions and context.draw and context.draw < len(questions):
-        ranges = context.ranges or ()
-        if not ranges:
-            sample_idxs = generator.sample(xrange(0, len(questions)), context.draw)
-            sample_idxs.sort() # keep order
-            result.extend(questions[idx] for idx in sample_idxs)
-        elif context.draw == len(ranges):
-            for r in ranges:
-                idx = generator.randint(r.start, r.end)
-                result.append(questions[idx])
-                generator = questionbank_random(context, user=user)
-        else:
-            result.extend(questions)
-    else:
-        result.extend(questions)
-    return result
+	result = []
+	questions = questions or context.questions
+	generator = questionbank_random(context, user=user)
+	if generator and questions and context.draw and context.draw < len(questions):
+		ranges = context.ranges or ()
+		if not ranges:
+			sample_idxs = generator.sample(xrange(0, len(questions)), context.draw)
+			sample_idxs.sort() # keep order
+			result.extend(questions[idx] for idx in sample_idxs)
+		else:
+			for r in ranges:
+				indices = xrange(r.start, r.end+1)
+				generated = generator.sample(indices, r.draw)
+				for idx in generated:
+					result.append(questions[idx])
+				generator = questionbank_random(context, user=user)
+	else:
+		result.extend(questions)
+	return result
