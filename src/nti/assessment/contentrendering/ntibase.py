88ddfbf2dae77018f91aa5e31b824fff9f618658
Sean Jones <sean.jones@nextthought.com>
2014-12-30 20:41:52 +0000

    Update nti.assesment's instance of LocalContentMixin to render the object if it has not been rendered when _asm_local_content is read and it is possible to render it. In general this should be compatable with past behavior. In the long run this should be merged into the base definition of LocalContentMixin from nti.dataserver.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@56196 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/assessment/contentrendering/ntibase.py
+++ src/nti/assessment/contentrendering/ntibase.py
@@ -21,6 +21,7 @@ from nti.contentfragments.interfaces import LatexContentFragment
 from nti.contentfragments.interfaces import ILatexContentFragment
 
 from nti.contentrendering.plastexpackages._util import _is_renderable
+from nti.contentrendering.plastexpackages._util import _asm_rendered_textcontent
 from nti.contentrendering.plastexpackages._util import _htmlcontent_rendered_elements
 from nti.contentrendering.plastexpackages._util import LocalContentMixin as _BaseLocalContentMixin
 
@@ -35,10 +36,42 @@ def aspveint(obj):
 	except (AssertionError, TypeError, ValueError):
 		raise ValueError("Bad positive integer value: %r" % obj)
 
+def _asm_local_sourcecontent(self, ignorable_renderables=()):
+	"""
+	Collects the tex source of the children of self. Returns a `unicode`
+	object.
+
+	:keyword ignorable_renderables: If given, a tuple (yes, tuple)
+		of classes. If a given child node is an instance of a
+		class in the tuple, it will be ignored and not returned.
+	"""
+
+	if not ignorable_renderables:
+		selected_children = self.childNodes
+	else:
+		selected_children = \
+			tuple(node for node in self.childNodes \
+				  if not isinstance(node, ignorable_renderables))
+
+	result = ILatexContentFragment(' '.join([x.source for x in selected_children]).strip())
+	return result
+
 class _LocalContentMixin(_BaseLocalContentMixin):
 	# SAJ: HACK. Something about naqvideo and _LocalContentMixin? ALl the parts
 	# and solutions from this module are excluded from rendering
 	_asm_ignorable_renderables = ()
+
+	# SAJ: The only time the following method is executed is prior to initial
+	# rendering of the element. The _after_render method of the parent class
+	# overwrites _asm_local_content to contain the filtered rendered content
+	# instead of the following method.
+	@readproperty
+	def _asm_local_content(self):
+		if self.renderer and _is_renderable(self.renderer, self.childNodes):
+			result = _asm_rendered_textcontent(self, self._asm_ignorable_renderables)
+		else:
+			result = _asm_local_sourcecontent(self)
+		return result
 
 class _AbstractNAQPart(_LocalContentMixin, Base.Environment):
 
