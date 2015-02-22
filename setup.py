b707cd0059d2a65f6fde68eefc06c928f624cd5e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-22 01:46:15 +0000

    set deps
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment/trunk@59977 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -10,32 +10,35 @@ entry_points = {
 }
 
 TESTS_REQUIRE = [
-    'nose',
-    'nose-timer',
-    'nose-pudb',
-    'nose-progressive',
-    'nose2[coverage_plugin]',
-    'pyhamcrest',
-    'nti.testing',
-    'nti.nose_traceback_info',
+	'nose',
+	'nose-timer',
+	'nose-pudb',
+	'nose-progressive',
+	'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'nti.testing'
 ]
 
 setup(
-    name = 'nti.assessment',
-    version = VERSION,
-    author = 'Jason Madden',
-    author_email = 'jason@nextthought.com',
-    description = "Support for automated assessments",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'pyramid preference',
-    classifiers = [
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name = 'nti.assessment',
+	version = VERSION,
+	author = 'Jason Madden',
+	author_email = 'jason@nextthought.com',
+	description = "Support for automated assessments",
+	long_description = codecs.open('README.rst', encoding='utf-8').read(),
+	license = 'Proprietary',
+	keywords = 'pyramid preference',
+	classifiers = [
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: Implementation :: CPython'
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti',],
@@ -48,16 +51,19 @@ setup(
 		'zope.interface',
 		'zope.datetime',
 		'nti.contentfragments',
+		# 'nti.externalization',
+		'nti.ntiids',
 		'nti.openmath',
 		'nti.plasTeX',
-		'nti.schema'
+		'nti.schema',
+		'nti.nose_traceback_info',
 	],
 	extras_require={
-        'test': TESTS_REQUIRE,
-    },
-    dependency_links=[
-        'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
-        'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
-    ],
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+	],
 	entry_points=entry_points
 )
