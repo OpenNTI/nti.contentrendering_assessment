78a04b26249f1477f23865c2696a70e53efd4d2e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-04 15:57:25 +0000

    initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.assessment_contentrendering/trunk@60780 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ entry_points = {
 	"z3c.autoinclude.plugin": [
 		'target = nti.contentrendering',
 	],
-	"console_scripts": [
-		"nti_extract_assessments = nti.assessment.scripts.nti_task_policy_extractor:main"
-	],
 }
 
 TESTS_REQUIRE = [
@@ -24,14 +21,14 @@ TESTS_REQUIRE = [
 ]
 
 setup(
-	name = 'nti.assessment',
+	name = 'nti.assessment.contentrendering',
 	version = VERSION,
 	author = 'Jason Madden',
 	author_email = 'jason@nextthought.com',
-	description = "Support for automated assessments",
+	description = "Support for assessment authoring",
 	long_description = codecs.open('README.rst', encoding='utf-8').read(),
 	license = 'Proprietary',
-	keywords = 'Assessments',
+	keywords = 'Assessments Authoring Content',
 	classifiers = [
 		'Intended Audience :: Developers',
 		'Natural Language :: English',
@@ -42,50 +39,32 @@ setup(
 	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
-	namespace_packages=['nti',],
+	namespace_packages=['nti', 'nti.assessment'],
 	tests_require=TESTS_REQUIRE,
 	install_requires=[
 		'setuptools',
+		'Paste',
+		'PasteDeploy',
 		'persistent',
-		'pytz',
-		'dolmen.builtins',
-		'dm.zope.schema',
-		'persistent',
-		'plone.namedfile',
-		'repoze.lru',
 		'simplejson',
 		'six',
-		'sympy',
-		'zope.annotation',
-		'zope.app.file',
 		'zope.cachedescriptors',
 		'zope.component',
-		'zope.container',
-		'zope.dublincore',
 		'zope.interface',
-		'zope.datetime',
-		'zope.file',
-		'zope.location',
 		'zope.mimetype',
 		'zope.schema',
-		'nti.common',
+		'zope.security',
+		'zope.traversing',
+		'nti.assessment',
 		'nti.contentfragments',
-		'nti.coremetadata',
-		'nti.dataserver.core',
-		'nti.dataserver.fragments',
-		'nti.dublincore',
+		'nti.contentrendering',
 		'nti.externalization',
-		'nti.links',
-		'nti.ntiids',
-		'nti.openmath',
-		'nti.plasTeX',
-		'nti.schema'
+		'nti.plasTeX'
 	],
 	extras_require={
 		'test': TESTS_REQUIRE,
 	},
 	dependency_links=[
-		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
 		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
 	],
 	entry_points=entry_points
