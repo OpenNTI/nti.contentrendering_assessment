6b9adae2b3e0fafd5641b6f0a87828c0f372cbaa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-06-07 23:26:25 +0000

    fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering_assessment/trunk@90053 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering_assessment/tests/test_ntirenderable.py
+++ src/nti/contentrendering_assessment/tests/test_ntirenderable.py
@@ -304,7 +304,7 @@ class TestRenderables(AssessmentRenderingTestCase):
 										  },
 							'href': 'index.html'}
 			del obj['Signatures']
-			remove_keys(obj, 'CreatedTime', 'Last Modified', 'Version')
+			remove_keys(obj, 'CreatedTime', 'Last Modified', 'version')
 			assert_that(obj, is_(exp_value))
 
 	def test_assessment_index_with_assignment(self):
@@ -427,7 +427,7 @@ class TestRenderables(AssessmentRenderingTestCase):
 						 'href': 'index.html'}
 
 			remove_keys(obj, 'ID', 'Signatures', 'CreatedTime', 'Last Modified',
-						'publishBeginning', 'publishEnding', 'Version')
+						'publishBeginning', 'publishEnding', 'version')
 			obj = json.dumps(obj, indent=4, sort_keys=True) 	
 			exp_value = json.dumps(exp_value, indent=4, sort_keys=True)
 			assert_that(obj, is_(exp_value))
