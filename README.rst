740d6f43c44b0ada978fd0418d03065b75bd087c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-03 03:41:12 +0000

    add supporting files;
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering_assessment/trunk@116432 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ README.rst
@@ -0,0 +1,9 @@
+=================================
+ nti.contentrendering_assessment
+=================================
+
+.. image:: https://travis-ci.org/NextThought/nti.contentrendering_assessment.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.contentrendering_assessment
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.contentrendering_assessment/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.contentrendering_assessment?branch=master
--- setup.py
+++ setup.py
@@ -4,66 +4,68 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.contentrendering',
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.contentrendering',
+    ],
 }
 
 TESTS_REQUIRE = [
-	'nose',
-	'nose-timer',
-	'nose-pudb',
-	'nose-progressive',
-	'nose2[coverage_plugin]',
-	'pyhamcrest',
-	'nti.testing'
+    'nti.testing',
+    'zope.testrunner',
 ]
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name = 'nti.contentrendering_assessment',
-	version = VERSION,
-	author = 'Jason Madden',
-	author_email = 'jason@nextthought.com',
-	description = "Support for assessment authoring",
-	long_description = codecs.open('README.rst', encoding='utf-8').read(),
-	license = 'Proprietary',
-	keywords = 'Assessments Authoring Content',
-	classifiers = [
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	tests_require=TESTS_REQUIRE,
-	install_requires=[
-		'setuptools',
-		'Paste',
-		'PasteDeploy',
-		'persistent',
-		'simplejson',
-		'six',
-		'zope.cachedescriptors',
-		'zope.component',
-		'zope.interface',
-		'zope.mimetype',
-		'zope.schema',
-		'zope.security',
-		'zope.traversing',
+    name='nti.contentrendering_assessment',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="Support for assessment authoring",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    url="https://github.com/NextThought/nti.contentrendering_assessment",
+    license='Apache',
+    keywords='assessments authoring content',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
 		'nti.assessment',
 		'nti.contentfragments',
 		'nti.contentrendering',
 		'nti.externalization',
 		'nti.mimetype',
 		'nti.plasTeX',
-		'nti.property'
-	],
-	extras_require={
-		'test': TESTS_REQUIRE,
-	},
-	entry_points=entry_points
+		'nti.property',
+        'Paste',
+        'PasteDeploy',
+        'persistent',
+        'simplejson',
+        'six',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.interface',
+        'zope.mimetype',
+        'zope.schema',
+        'zope.security',
+        'zope.traversing',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points
 )
